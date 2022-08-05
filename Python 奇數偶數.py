ac94ddfb48bdd574420bedb7bc2e8e88bb144ef5
Patrick Fang <98370112+Patrick0105@users.noreply.github.com>
2022-08-05 22:21:51 +0800

    asd

--- Python 奇數偶數.py
+++ Python 奇數偶數.py
@@ -1,5 +1,7 @@
 if __name__ =='__main__':
     product_price = [15900, 30900, 32900, 16900, 13900]
+    #3 0 48800 24400.0 (15900,32900)
+    #4 1 47800 23900.0 (30900,16900)
     result = 0
     num = 0
     avg = 0
@@ -7,15 +9,16 @@ if __name__ =='__main__':
     counter = int(input('宣告成請選擇只看偶數(0)奇數(1)：'))
 
     while num <= len_product:
-            # if counter == 1: #只看奇數
-            #     num += 2
-            #     result += product_price[num]
-            #     avg += 1
-            if counter == 0: #只看偶數
-                avg += 1
-                result = product_price[num]
-                print(result)
-                num += 2
-    # print(f'加總結果{result}')
-    # print(f'加總平均結果{result / avg}')
-    
+        try:
+            if counter == 0:
+                    result += product_price[num]
+                    num += 2
+            elif counter == 1:
+                    num += 1
+                    result += product_price[num]
+                    num += 1
+            avg += 1
+        except:
+            break
+    print(f'加總結果{result}')
+    print(f'加總平均結果{result / avg}')
