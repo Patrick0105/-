c49aa240e562b10e45126d3b8eb18152c7b97bbb
Patrick0105 <patrick910105@gmail.com>
2022-08-06 11:31:05 +0800

    kk

--- R 奇數偶數.r
+++ R 奇數偶數.r
@@ -5,9 +5,14 @@ avg <- 0
 len_product <- '請輸入欲計算至第幾筆資料整數'
 len_product
 len_product <- scan(nmax = 2)
+3
+
+
 counter <- '請選擇只看奇數(1)偶數(2)'
 counter
 counter <- scan(nmax = 2)
+1
+
 #3 1 48800 24400
 #5 2 47800 23900
 while (num <= len_product[1]){
@@ -15,14 +20,13 @@ while (num <= len_product[1]){
         if (counter[1] == 1){
                 result <- result + product_price[num]
                 num <- num + 2
-                avg <- avg + 1
         }
         else if (counter[1] == 2){
                 num <- num + 1
                 result <- result + product_price[num]
                 num <- num + 1
-                avg <- avg + 1
         }
+        avg <- avg + 1
     }, warning = function(w){
         break()
     }, error = function(e){
